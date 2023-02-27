09f8d7389c9ca9dc618c8c46aebecbf2ad1bdf89
Chase <chaseaohill@gmail.com>
2023-02-27 16:48:45 -0500

    Completed project

--- Forecast_Codable/Forecast_Codable/Controllers/View Controller/DayDetailsViewController.swift
+++ Forecast_Codable/Forecast_Codable/Controllers/View Controller/DayDetailsViewController.swift
@@ -18,27 +18,48 @@ class DayDetailsViewController: UIViewController {
     @IBOutlet weak var currentDescriptionLabel: UILabel!
     
     //MARK: - Properties
+    var days: [Day] = []
+    var forecastData: TopLevelDictionary?
     
     //MARK: - View Lifecyle
     override func viewDidLoad() {
         super.viewDidLoad()
+        dayForcastTableView.delegate = self
+        dayForcastTableView.dataSource = self
+        DayController.fetchDays { forecastData in
+            guard let forecastData = forecastData else { return }
+            self.forecastData = forecastData
+            self.days = forecastData.days
+            DispatchQueue.main.async {
+                self.dayForcastTableView.reloadData()
+                self.updateViews()
+            }
+        }
     }
     
     func updateViews() {
-        
+        let day = days[0]
+        cityNameLabel.text = forecastData?.cityName
+        currentTempLabel.text = String(day.temp)
+        currentLowLabel.text = String(day.lowTemp)
+        currentHighLabel.text = String(day.highTemp)
+        currentDescriptionLabel.text = day.weather.description
     }
 }
 
 //MARK: - Extenstions
 extension DayDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 44
+        return days.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "dayC3ll", for: indexPath) as? DayForcastTableViewCell else {return UITableViewCell()}
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath) as? DayForcastTableViewCell else { return UITableViewCell() }
+        
+        let indexPath = days[indexPath.row]
+        cell.updateViews(day: indexPath)
         
         return cell
     }
 }
-
